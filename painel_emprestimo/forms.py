63f575d8bcb97d5e28f1ea94336c08bf30227ce0
Rafael <contato.rafael2023@gmail.com>
2025-03-07 20:23:23 -0300

    alterando o label forms

--- painel_emprestimo/forms.py
+++ painel_emprestimo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class Emprestimo_form(forms.Form):
-    renda = forms.DecimalField(label='Renda Mensal R$', max_digits=8, decimal_places=2)
+    renda = forms.DecimalField(label='Renda anual R$', max_digits=8, decimal_places=2)
     emprestimo =  forms.DecimalField(label='Empréstimo desejado',max_digits=8, decimal_places=2)
